0ba2f582c8844351726819ed38a07885a7c496e9
Vanchenko <vanchenkoev@pakline.com.ua>
2023-07-08 16:32:45 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,9 @@
 import { nanoid } from "nanoid";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import css from './ContactForm.module.css'
 import { addContacts } from "redux/contactsSlice";
+import { getContacts } from "redux/selectors";
 
 const nameInputId = nanoid();
 const numberInputId = nanoid();
@@ -11,6 +12,7 @@ export const ContactForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const dispatch = useDispatch();
+  const listContacts = useSelector(getContacts);
 
   // Отвечает за обновление состояния
   const handleChange = evt => {
@@ -26,11 +28,16 @@ export const ContactForm = () => {
   // Вызывается при отправке формы
   const handleSubmit = evt => {
     evt.preventDefault();
+
     const data = {
       name: name,
       number: number,
+    };
+    if (listContacts.some(item => item.name === data.name)) {
+      alert('This contact has already been added.');
+      return;
     }
-    dispatch(addContacts(data))
+    dispatch(addContacts(data));
     setName('');
     setNumber('');
   };
@@ -44,7 +51,7 @@ export const ContactForm = () => {
         name="name"
         value={name}
         placeholder="Enter name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         onChange={handleChange}
@@ -57,7 +64,7 @@ export const ContactForm = () => {
         name="number"
         value={number}
         placeholder="Enter phone number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
         onChange={handleChange}
